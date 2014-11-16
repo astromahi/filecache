7369ef1668ae24f8fce099621b8f61c3dc0e791f
Mahendra Kathirvel <astromahi@gmail.com>
2014-11-16 21:41:22 +0530

    Add initial commit

---
+++ filecache_test.go
@@ -0,0 +1,5 @@
+//Copyright 2014 Mahendra Kathirvel. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package filecache
